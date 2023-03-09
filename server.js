f7a93e74fa8f307d5562347bc98a180dd66cf98e
Xhway <dhway20@gmail.com>
2023-03-08 19:16:02 -0500

    changed api path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
-const apiRoutes = require('../Develop/routes/apiRoutes');
-const htmlRoutes = require('../Develop/routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
