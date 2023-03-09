091a2bb06c51144f51fbc34268a8a4447b3ca884
Xhway <dhway20@gmail.com>
2023-03-08 19:20:34 -0500

    updated path

--- helpers/saved.js
+++ helpers/saved.js
@@ -7,10 +7,10 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 class Saved{
     read(){
-        return readFileAsync("../Develop/db/db.json", "utf8")
+        return readFileAsync("./db/db.json", "utf8")
     }
     write(note){
-        return writeFileAsync("../Develop/db/db.json", JSON.stringify(note))
+        return writeFileAsync("./db/db.json", JSON.stringify(note))
     }
     addNote(note){
         const {title, text} = note
